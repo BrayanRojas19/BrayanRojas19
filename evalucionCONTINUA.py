4bc35c066aa3286b9fdc37a68b26b66651bff719
BrayanRojas19 <110873269+BrayanRojas19@users.noreply.github.com>
2022-10-19 12:22:34 -0400

    Add files via upload

---
+++ evalucionCONTINUA.py
@@ -0,0 +1,14 @@
+#tradicional
+def EvaluacionContinuaTradicional(nota1,nota2,nota3,nota4)
+    promedioTotal=[]
+    promedio=0
+  for i in range(len(nota1)):
+     promedio=(nota1[i]*0.20+nota2[i]*0.25+nota3[i]*0.25+nota4[i]*0.30)
+     promedioTotal.append(promedio)
+     return promedioTotal
+#numpy     
+def EvaluacioncontinuaN(nota1,nota2,nota3,nota4)
+     promedio=(nota1*0.20+nota2*0.25+nota3*0.25+nota4*0.30)
+     return promedioTotal
+  
+  
